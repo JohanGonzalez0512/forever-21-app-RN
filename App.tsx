b9cd7a91b3f1d912badf4b345f0d89aabccdc020
Johan Gonzalez <jopi20101@gmail.com>
2022-10-05 23:21:13 -0500

    Done (Redux): Redux instalation

--- App.tsx
+++ App.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { NavigationStack } from './src/navigation/NavigationStack';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
 
 
 export const App = () => {
   return (
 
-    // <Provider store={store}>
-    <NavigationContainer>
-
-
-      <NavigationStack />
-      {/* <SplashScreen /> */}
-    </NavigationContainer>
-    // </Provider >
+    <Provider store={store}>
+      <NavigationContainer>
+        <NavigationStack />
+      </NavigationContainer>
+    </Provider >
   );
 };
