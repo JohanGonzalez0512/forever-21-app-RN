743db195b109eacb44a48654767c786a2d2c43ca
Johan Gonzalez <jopi20101@gmail.com>
2022-10-16 23:45:39 -0500

    Done (Redux): Products Added

---
+++ src/store/products/thunks.ts
@@ -0,0 +1,87 @@
+import { Dispatch } from 'redux';
+import { forever21Api } from '../../api';
+import { Alert } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../navigation/ProductsStack';
+import { addProduct } from './productSlice';
+import { existenceResp } from '../../interfaces';
+interface DataChecking {
+    navigation: NativeStackNavigationProp<RootStackParamList, "CaptureScreen", undefined>
+    code: string
+}
+
+export const startCheckingExistence = ({ navigation, code }: DataChecking) => {
+    return async (dispatch: Dispatch) => {
+        try {
+            console.log(code);
+
+            if (code.length > 12) {
+                navigation.navigate('ProductsScreen');
+                Alert.alert(
+                    "Error",
+                    "El código de barras o QR es demasiado largo",
+                    [
+
+                        { text: "OK" }
+                    ],
+                )
+                return;
+            }
+
+            const { data } = await forever21Api.get<existenceResp>(`products/existence/${code}`);
+
+            if (data.existance) {
+
+                dispatch(addProduct(data.product));
+                navigation.navigate('ProductsScreen');
+            }
+
+
+            // } else {
+            //     console.log('jejej form')
+            //     // navigation.navigate('FormScreen');
+            // }
+
+
+
+
+        } catch (error: any) {
+            console.log(error.response.data.message);
+            navigation.navigate('ProductsScreen');
+            Alert.alert(
+                "Error",
+                "Hubo un error al verificar el producto",
+                [
+
+                    { text: "OK" }
+                ],
+            )
+
+        }
+    };
+};
+
+
+export const startCreatingProduct = ({ navigation, code }: DataChecking) => {
+    return async (dispatch: Dispatch) => {
+        try {
+
+
+            console.log(code)
+
+
+            // const { data } = await forever21Api.post<User>('/auth/login', dataUser);
+
+
+            // dispatch();
+
+
+
+
+        } catch (error: any) {
+            console.log(error.response.data.message);
+
+        }
+    };
+};
+
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authSlice } from './auth';
 import { permissionSlice } from './permissions';
+import { productSlice } from './products/productSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authSlice.reducer,
         permission: permissionSlice.reducer,
+        product: productSlice.reducer,
        
     },
 });
