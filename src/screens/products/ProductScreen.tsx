f0ded3d051f6556c0e42ce5eb3c382ac092c1725
Johan Gonzalez <jopi20101@gmail.com>
2022-10-16 23:47:08 -0500

    Done (Product): Product Screen Done

--- src/screens/products/ProductScreen.tsx
+++ src/screens/products/ProductScreen.tsx
@@ -1,19 +1,65 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native';
+import React, { FC } from 'react'
+import { View, StyleSheet, FlatList } from 'react-native';
+import { Header, NoData } from '../../components/ui';
+import { Buttons, ProductCard } from '../../components/products';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../navigation/ProductsStack';
+import { useAppSelector } from '../../hooks/hooks';
 
-export const ProductScreen = () => {
 
-    const navigation = useNavigation<any>();
+
+interface Props extends NativeStackScreenProps<RootStackParamList, 'ProductsScreen'> { }
+export const ProductScreen: FC<Props> = ({ navigation }) => {
+
+    const { products } = useAppSelector(state => state.product)
+
+
 
     return (
-        <View>
-            <Text>ProductScreen</Text>
+        <View style={styles.container}>
+            <Header title='Productos' />
 
-            <Button
-                title="Go to CaptureScreen"
-                onPress={() => navigation.navigate('CaptureScreen')}
+
+            {
+                products.length > 0
+                    ?
+                    <FlatList
+                        style={styles.flatList}
+                        showsVerticalScrollIndicator={false}
+                        data={products}
+                        keyExtractor={(item, index) => item.name + index}
+                        renderItem={({ item }) => <ProductCard product={item} />}
+                        ListFooterComponent={<View style={{ height: 100 }} />}
+                    />
+                    :
+                    <NoData
+                        path={require('../../assets/no-data.png')}
+                        text='No hay productos'
+                    />
+
+
+            }
+
+
+
+
+            <Buttons
+                handlePressUpload={() => console.log('left')}
+                handlePressAdd={() => navigation.navigate('CaptureScreen')}
             />
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    flatList: {
+        flex: 1,
+        marginTop: 20,
+    },
+
+
+});
